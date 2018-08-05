2e4a69d21565a0f15ac6a3370c0fe2c894b22247
Tom <t.oliver.ball@protonmail.ch>
2018-08-05 10:30:28 +0100

    Recursive data types are cool..

--- Hutton-ProgrammingInHaskell/ch8.hs
+++ Hutton-ProgrammingInHaskell/ch8.hs
@@ -1,6 +1,5 @@
 -- Ex 1/
-data Nat = Zero | Succ Nat 
-           deriving (Eq, Show)
+data Nat = Zero | Succ Nat deriving (Eq, Show)
 
 int2nat :: Int -> Nat
 int2nat n | n < 0     = error ""
@@ -8,13 +7,50 @@ int2nat n | n < 0     = error ""
           | otherwise = Succ (int2nat $ n - 1)
 
 nat2int :: Nat -> Int
-nat2int Zero = 0
+nat2int Zero     = 0
 nat2int (Succ n) = 1 + nat2int n
 
 add :: Nat -> Nat -> Nat
-add Zero     m     = m
-add (Succ n) m     = add n (Succ m)
+add Zero     m = m
+add (Succ n) m = add n (Succ m)
 
 mult :: Nat -> Nat -> Nat
 mult _ Zero     = Zero
 mult n (Succ m) = add n (mult n m)
+
+
+-- Ex 2/
+data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving (Eq, Show)
+
+-- More efficient since comparison value requires only one computation.
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y)                = x == y
+occurs x (Node l y r) | eq == EQ = True
+                      | eq == LT = occurs x l
+                      | eq == GT = occurs x r
+                      where
+                        eq = compare x y
+
+
+-- Ex 3/
+data Tree' a = Leaf' a | Node' (Tree' a) (Tree' a) deriving (Eq, Show)
+
+nleaves :: Tree' a -> Int
+nleaves (Leaf' x)   = 1
+nleaves (Node' l r) = (nleaves l) + (nleaves r)
+
+balanced :: Tree' a -> Bool
+balanced (Leaf' x)   = True
+balanced (Node' l r) = diff < 2
+                      where
+                        diff = abs $ (nleaves l) - (nleaves r)
+
+
+-- Ex 4/
+balance :: [a] -> Tree' a
+balance []  = error ""
+balance [x] = Leaf' x
+balance xs  = Node' (balance l) (balance r)
+              where
+                (l, r) = (take n xs, drop n xs)
+                n      = (length xs) `div` 2
