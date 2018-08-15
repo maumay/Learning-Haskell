56ecf760fa2f8ee0d0f6b9d8f9e37a3afbf82850
Tom <t.oliver.ball@protonmail.ch>
2018-08-15 18:01:55 +0100

    Working on Sudoku solver

--- sudoku.hs
+++ sudoku.hs
@@ -1,7 +1,8 @@
 import qualified Data.Set as Set
 import Text.Read (readMaybe)
 import Data.List (elemIndex)
-import Data.Maybe (fromJust, isNothing)
+import Data.Maybe (fromJust, isNothing, isJust)
+
 
 data Entry = E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 | E9
              deriving (Eq, Ord)
@@ -23,15 +24,43 @@ string2entry x | isNothing y || outRange num = error ""
                 outRange = \n -> n < 1 || n > 9
                 num      = fromJust y
 
--- Check if some list contains the full set of entries.
-completeEntrySet :: [Entry] -> Bool
-completeEntrySet xs = (Set.size $ Set.fromList xs) == 9
-
 
 type GridSquare = Maybe Entry
+
+isLegalSquareSet :: [GridSquare] -> Bool
+isLegalSquareSet xs = (length ys) == (Set.size $ Set.fromList ys)
+                      where
+                          ys = filter isJust xs
+
 
 completeSquareSet :: [GridSquare] -> Bool
 completeSquareSet xs = (Set.size $ Set.fromList ys) == 9
                        where
-                        ys = filter (Nothing /=) xs
+                        ys = filter isJust xs
 
+type Grid  = [GridSquare]
+type Row   = [GridSquare]
+type Col   = [GridSquare]
+type Block = [GridSquare]
+
+
+rows :: Grid -> [Row]
+rows [] = []
+rows xs = (take 9 xs) : (rows $ drop 9 xs)
+
+cols :: Grid -> [Col]
+cols g  = map (coln g) [1..9]
+
+coln :: Grid -> Int -> Col
+coln [] n  = []
+coln g  n  = (head $ drop n g) : (coln (drop 9 g) n)
+
+-- blockn :: Grid -> Int -> Int -> Block
+-- blockn g n m =  
+
+
+-- inGridRange :: Int -> Bool
+-- inGridRange x = 0 <= x && x < 81
+
+-- indexGrid :: Grid -> Int -> GridSquare
+-- indexGrid g (row, col) = g !! ((row - 1) * 9 + (col - 1)) 
