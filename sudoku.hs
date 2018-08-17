25ff44e0d17d3fc4956a0ffa7eb92d06f53b95ec
Tom <t.oliver.ball@protonmail.ch>
2018-08-17 16:40:59 +0100

    Decided to just use ints as the grid entries

--- sudoku.hs
+++ sudoku.hs
@@ -1,59 +1,47 @@
 import qualified Data.Set as Set
-import Text.Read (readMaybe)
-import Data.List (elemIndex)
-import Data.Maybe (fromJust, isNothing, isJust)
+-- import Text.Read (readMaybe)
+-- import Data.List (elemIndex)
+import Data.Maybe (isNothing, isJust)
 
 
-data Entry = E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 | E9
-             deriving (Eq, Ord)
-
-allEntries :: [Entry]
-allEntries = [E1, E2, E3, E4, E5, E6, E7, E8, E9]
-
-instance Show Entry where
-    show x = show $ (fromJust $ elemIndex x allEntries) + 1
-
--- Don't understand the Read typeclass fully yet :)
--- utilising laziness here for things to make sense
--- would be errors in a strict world.
-string2entry :: String -> Entry
-string2entry x | isNothing y || outRange num = error ""
-               | otherwise                   = allEntries !! (num - 1)
-               where
-                y        = readMaybe x :: Maybe Int
-                outRange = \n -> n < 1 || n > 9
-                num      = fromJust y
-
-
-type GridSquare = Maybe Entry
+type GridSquare = Maybe Int
 
 isLegalSquareSet :: [GridSquare] -> Bool
 isLegalSquareSet xs = (length ys) == (Set.size $ Set.fromList ys)
                       where
                           ys = filter isJust xs
 
-
-completeSquareSet :: [GridSquare] -> Bool
-completeSquareSet xs = (Set.size $ Set.fromList ys) == 9
-                       where
-                        ys = filter isJust xs
-
 type Grid  = [GridSquare]
 type Row   = [GridSquare]
 type Col   = [GridSquare]
 type Block = [GridSquare]
 
-
+-- Function to retrieve rows in a grid.
 rows :: Grid -> [Row]
 rows [] = []
 rows xs = (take 9 xs) : (rows $ drop 9 xs)
 
+-- Function to retrieve columns in a grid.
 cols :: Grid -> [Col]
-cols g  = map (coln g) [1..9]
+cols g  = map (coln g) [0..8]
 
 coln :: Grid -> Int -> Col
 coln [] n  = []
 coln g  n  = (head $ drop n g) : (coln (drop 9 g) n)
+
+-- Function to retrieve blocks in a grid.
+blocks :: Grid -> [Block]
+blocks g = map (blockn g) blockstarts
+
+blockstarts :: [Int]
+blockstarts = [27*x + 3*y | x <-[0..2], y <-[0..2]]
+
+blockn :: Grid -> Int -> Block
+blockn g start = [g !! (start + x + 9*y) | x <-[0..2], y <-[0..2]]
+
+-- Function which checks if a grid is in a legal state.
+isLegalGrid :: Grid -> Bool
+isLegalGrid g = all isLegalSquareSet $ concat [rows g, cols g, blocks g]
 
 -- blockn :: Grid -> Int -> Int -> Block
 -- blockn g n m =  
