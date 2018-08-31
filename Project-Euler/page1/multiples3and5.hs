19b96412fd26a824a11f408e63d7ab02dd09e855
ThomasB <Thomas.Ball@lhasalimited.org>
2018-08-31 17:33:49 +0100

    tidying repo

--- Project-Euler/page1/multiples3and5.hs
+++
@@ -1,7 +0,0 @@
-multiples :: Int -> Int
-multiples n = sum (filter (\x -> p3 x || p5 x) [1..m])
-              where m = n - 1
-                    p3 = \x -> x `mod` 3 == 0
-                    p5 = \x -> x `mod` 5 == 0
-
-main = putStrLn (show (multiples 1000)) 
--- Project-Euler/page1/p4-largestPalindrome.hs
+++
@@ -1,4 +0,0 @@
-import GHC.Exts (sortWith)
-
-products =  let nrange = reverse [900..999] in sortWith (\x -> -x) [x*y | x <- nrange, y <- nrange]
-main = putStrLn $ head $ filter (\xs -> xs == (reverse xs)) $ map show products
